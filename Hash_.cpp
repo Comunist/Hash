a831adbe5416103f46d604ab3c4cf8bedbcfeab9
Ilya <iliya.yurkevic@mail.ru>
2019-09-08 14:49:14 +0300

    Добавьте файлы проекта.

---
+++ Hash_.cpp
@@ -0,0 +1,79 @@
+﻿#include <windows.h>
+#pragma comment(lib, "advapi32")
+#include "iostream"
+#include "wincrypt.h"
+#include "WinCryptEx.h"
+
+#define GR3411LEN  64
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	HCRYPTPROV hProv;
+	HCRYPTHASH hHash;
+	DWORD cbHash = 0;
+	BYTE rgbHash[GR3411LEN];
+	CHAR rgbDigits[] = "0123456789ABCDEF";
+
+	// Инициализация контекста криптопровайдера
+	if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_GOST_2012_256, CRYPT_VERIFYCONTEXT))
+	{
+		cout << "Ошибка инициализации контекста криптопровайдера" << endl;
+		return 1;
+	}
+
+	cout << "Cryptographic provider initialized" << endl;
+
+	// Cоздание хеш-объекта
+	if (!CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, &hHash))
+	{
+		CryptReleaseContext(hProv, 0);
+		cout << "Ошибка создания хеш-объекта" << endl;
+		return 1;
+	}
+
+	cout << "Hash created" << endl;
+
+	// Тестовые данные для хеширования
+	char string[] = "Hello crypto world";
+	DWORD count = strlen(string);
+
+	cout << "Get Input Data: " << string << endl;
+
+	// Передача хешируемых данных хэш-объекту.
+	if (!CryptHashData(hHash, (BYTE*)string, count, 0))
+	{
+		CryptReleaseContext(hProv, 0);
+		CryptDestroyHash(hHash);
+		cout << "Ошибка передачи хешируемых данных хэш-объекту" << endl;
+		return 1;
+	}
+
+	cout << "Hash data loaded\n";
+
+	// Получение хеш-значения
+	cbHash = GR3411LEN;
+	if (!CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, &cbHash, 0))
+	{
+		CryptReleaseContext(hProv, 0);
+		CryptDestroyHash(hHash);
+		cout << "CryptGetHashParam failed" << endl;
+	}
+
+	// Вывод на экран полученного хеш-значения
+	cout << "Hash value: ";
+	for (int i = 0; i < cbHash; i++)
+	{
+		printf("%c%c ", rgbDigits[rgbHash[i] >> 4],
+			rgbDigits[rgbHash[i] & 0xf]);
+	}
+	cout << "\n\n";
+
+	// Освобождение
+	CryptDestroyHash(hHash);
+	CryptReleaseContext(hProv, 0);
+
+	return 0;
+}
